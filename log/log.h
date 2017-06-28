0c8bae93725f19e7fb4fccac2670ad0a0a638770
Wiki Wang <wikiwang@live.com>
2017-06-28 14:12:42 +0800

    Fix header.

--- log/log.h
+++ log/log.h
@@ -38,19 +38,64 @@ int log_close();
 
 #endif
 
+#if LOG_LEVEL <= 0
+
 #define logg_debug(fmt, ...) logg(0, fmt, ##__VA_ARGS__)
 #define logm_debug(fmt, ...) logm(0, fmt, ##__VA_ARGS__)
+
+#else
+
+#define logg_debug(fmt, ...)
+#define logm_debug(fmt, ...)
+
+#endif
+
+#if LOG_LEVEL <= 1
 
 #define logg_info(fmt, ...) logg(1, fmt, ##__VA_ARGS__)
 #define logm_info(fmt, ...) logm(1, fmt, ##__VA_ARGS__)
 
+#else
+
+#define logg_info(fmt, ...)
+#define logm_info(fmt, ...)
+
+#endif
+
+#if LOG_LEVEL <= 2
+
 #define logg_warning(fmt, ...) logg(2, fmt, ##__VA_ARGS__)
 #define logm_warning(fmt, ...) logm(2, fmt, ##__VA_ARGS__)
+
+#else
+
+#define logg_warning(fmt, ...)
+#define logm_warning(fmt, ...)
+
+#endif
+
+#if LOG_LEVEL <= 3
 
 #define logg_critical(fmt, ...) logg(3, fmt, ##__VA_ARGS__)
 #define logm_critical(fmt, ...) logm(3, fmt, ##__VA_ARGS__)
 
+#else
+
+#define logg_critical(fmt, ...)
+#define logm_critical(fmt, ...)
+
+#endif
+
+#if LOG_LEVEL <= 4
+
 #define logg_fatal(fmt, ...) logg(4, fmt, ##__VA_ARGS__)
 #define logm_fatal(fmt, ...) logm(4, fmt, ##__VA_ARGS__)
+
+#else
+
+#define logg_fatal(fmt, ...)
+#define logm_fatal(fmt, ...)
+
+#endif
 
 #endif // LOG_MAX_LEVEL
